970ac6a8f2fe5fd9b6b8807db898b412e49c0744
Aleksandr Klachkevich <Aleksandr Klachkevich>
2016-10-26 04:17:14 +0300

    add window

---
+++ script/show.js
@@ -0,0 +1,9 @@
+function show(state){
+
+    document.getElementById('window').style.display = state;
+    document.getElementById('wrap').style.display = state;
+}
+/*function price(a){
+  var divPrice = document.getElementById('price'),
+  divPrice.innerHTML = a;
+}*/
