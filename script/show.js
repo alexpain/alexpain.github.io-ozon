216ef565dccdd2bb97ef26b28996807dbfddbb9e
Aleksandr Klachkevich <Aleksandr Klachkevich>
2016-10-26 11:53:40 +0300

    add price

--- script/show.js
+++ script/show.js
@@ -3,7 +3,8 @@ function show(state){
     document.getElementById('window').style.display = state;
     document.getElementById('wrap').style.display = state;
 }
-/*function price(a){
-  var divPrice = document.getElementById('price'),
-  divPrice.innerHTML = a;
-}*/
+function price(){
+  var divPrice = document.getElementById('price');
+  var a = document.getElementById('sum');
+  divPrice.innerHTML = a.value;
+}
